7687563f9069a9e9b81149dc871c40c167be3e66
Yadati26 <GabrielSrour154@gmail.com>
2025-05-15 11:13:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from alpaca_trade_api.rest import REST
+
+API_KEY = os.getenv("APCA_API_KEY_ID")
+API_SECRET = os.getenv("APCA_API_SECRET_KEY")
+BASE_URL = "https://paper-api.alpaca.markets"
+
+api = REST(API_KEY, API_SECRET, BASE_URL)
+account = api.get_account()
+
+print(f"Account status: {account.status}")
